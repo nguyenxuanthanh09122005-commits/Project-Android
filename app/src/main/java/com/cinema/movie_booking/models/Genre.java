140da243d50eac0fa3685b3774c7f3599c61f794
diepdv8168 <dvdiep1234@gmail.com>
2026-05-25 01:44:16 +0700

    fix: move project files out of subfolder to root

--- app/src/main/java/com/cinema/movie_booking/models/Genre.java
+++
@@ -1,9 +0,0 @@
-package com.cinema.movie_booking.models;
-
-public class Genre {
-    private Long genreId;
-    private String genreName;
-
-    public Long getGenreId() { return genreId; }
-    public String getGenreName() { return genreName; }
-}
